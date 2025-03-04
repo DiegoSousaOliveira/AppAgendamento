f26f68140da69b6398a2f6d59de292467d57b1a6
Diego de Sousa Oliveira <diegosousa0056@gmail.com>
2025-03-04 20:51:29 -0300

    removendo a classe AgendamentoService e adicionando seus atributos e métodos na classe Hospital. Além de add novos métodos.

--- backend/classes.py
+++ backend/classes.py
@@ -17,8 +17,6 @@ class Paciente(Pessoa):
     
     def atualizar_dados(self, cpf: str, nome: str, email: str, senha: str, telefone): pass
 
-    def autenticar(self, email: str, cpf: str, senha: str): pass
-
 
 class Medico(Pessoa):
     def __init__(self, nome: str, cpf: str, email: str, telefone: str, especialidade: str):
@@ -27,20 +25,16 @@ class Medico(Pessoa):
 
 
 class Endereco:
-    def __init__(self, rua: str, numero: str, bairro: str, cidade: str, estado: str, cep: str):
-        self.rua = rua
-        self.numero = numero
-        self.bairro = bairro
-        self.cidade = cidade
-        self.estado = estado
-        self.cep = cep
+    def __init__(self, rua: str, numero: str, bairro: str, cidade: str, estado: str, complemento: str,cep: str):
+        self.rua:str        = rua
+        self.numero:str     = numero
+        self.bairro:str     = bairro
+        self.cidade:str     = cidade
+        self.estado:str     = estado
+        self.complemeto:str = complemento
+        self.cep:str        = cep
 
-
-class Hospital:
-    def __init__(self, nome: str, endereco: Endereco, telefone: str):
-        self.nome = nome
-        self.endereco = endereco
-        self.telefone = telefone
+    def atualizar_dados(self, rua: str, numero: str, bairro: str, cidade: str, estado: str, cep: str): pass
 
 
 class Consulta:
@@ -51,19 +45,39 @@ class Consulta:
         self.paciente = paciente
 
 
-class AgendamentoService:
-    def __init__(self):
+class Hospital:
+    def __init__(self, nome: str, endereco: Endereco, telefone: str):
+        self.nome:str               = nome
+        self.telefone:str           = telefone
+        self.endereco:Endereco      = endereco
         self.medicos: List[Medico]      = []
         self.consultas: List[Consulta]  = []
         self.pacientes: List[Paciente]  = []
 
-    def marcar_consulta(self, consulta: Consulta):pass
+    def cadastrar_medico(self, medico: Medico):pass
+
+    def cadastrar_paciente(self, paciente: Paciente):pass
+
+    def remover_medico(self, medico: Medico):pass
+
+    def remover_paciente(self, paciente: Paciente):pass
     
-    def cancelar_consulta(self, usuario_cpf: str):pass
+    def listar_medicos(self):pass
+
+    def listar_pacientes(self):pass
 
     def listar_consultas(self):pass
 
+    def buscar_medico_por_especialidade(self, especialidade: str):pass
+
+    def buscar_medico_por_cpf(self, cpf: str):pass
+
+    def buscar_paciente_por_cpf(self, cpf: str):pass
+
+    def marcar_consulta(self, consulta: Consulta):pass
+    
+    def cancelar_consulta(self, cpf: str):pass
+
     def buscar_consulta_por_horario(self, data: date, hora: time):pass
 
-    def buscar_consultas_por_paciente(usuario_cpf):pass
-
+    def buscar_consultas_por_paciente(cpf):pass
